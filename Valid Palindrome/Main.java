c7dc0ca3e3fc93e00df2a5629e2a62d78cf2df3a
isabella.mattua <isa_mattua@hotmail.com>
2020-08-03 16:37:35 -0400

    Day 3

---
+++ Valid Palindrome/Main.java
@@ -0,0 +1,48 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution myS =new Solution();
+		
+		
+		System.out.println(myS.isPalindrome("A man, a plan, a canal: Panama"));
+		
+
+	}
+
+}
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        int newLetter = 0;
+        String word = "";
+        
+        while (newLetter < s.length()) {
+        	if (Character.isLetterOrDigit(s.charAt(newLetter))) {
+        		word = word + s.charAt(newLetter);
+        	}
+        	newLetter ++; 
+        }
+         word= word.toLowerCase();
+         System.out.println(word);
+    	
+         String newWord = "";
+         int letter  = word.length() -1;
+         while (letter >= 0) {
+         	if (Character.isLetterOrDigit(word.charAt(letter))) {
+         		newWord = newWord + word.charAt(letter);
+         	}
+         	
+             letter--;
+         }
+         
+         System.out.println(newWord);
+        if (newWord.equals(word)){
+            return true;
+        } else {
+            return false;
+        }
+ 
+        
+    }
+}
